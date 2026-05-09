04faf21417c840bf08f228ad66e5793cf21c0736
swastika2015 <jmswastika18409@gmail.com>
2026-05-09 08:37:54 +0530

    Add files via upload

---
+++ type_casting.py
@@ -0,0 +1,21 @@
+Name = "Swastika"
+Age = 12
+is_student = True
+Weight = 40.2
+
+#Data types
+print("type of Name", type(Name))
+
+print("type of Age", type(Age))
+
+print("type of is_student", type(is_student))
+
+print("type of Weight", type(Weight))
+
+#Typecasting
+
+Weight= int(Weight)
+print(" type of Weight", type (Weight))
+
+Age = str(Age)
+print("type of Age",type (Age))
